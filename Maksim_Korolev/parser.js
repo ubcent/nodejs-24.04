f936b0d46bbd286374154ea192b9bc3a2204f71e
KosmoPirat <kosmopirat@gmail.com>
2019-05-16 14:11:33 +0300

    done parser

---
+++ Maksim_Korolev/parser.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://120.su/', (err, req, html) => {
+    if (!err && req.statusCode === 200) {
+        const $ = cheerio.load(html);
+        for (let i = 0; i < 3; i++) {
+            console.log(`Новость: ${i + 1}\n${$('.entry-title').eq(i).text()}\n`);
+            console.log($('.entry-content').eq(i).find('p').text() + '\n');
+        }
+
+    } else {
+        console.log('Error: ' + err);
+    }
+});

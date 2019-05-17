d1ec160d377c271556b4a293bdfcbc3227eaded9
Nauronski <6602796@gmail.com>
2019-05-18 00:14:48 +0200

    lesson3Task1; added colors

--- NavrotskiAndrei/translatorYandex.js
+++ NavrotskiAndrei/translatorYandex.js
@@ -1,5 +1,6 @@
 const request =  require('request');
 const url = require('url');
+const colors = require('colors');
 const readline = require('readline');
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -7,15 +8,16 @@ const rl = readline.createInterface({
 });
 
 class Translator {
-	constructor(text, key = 'trnsl.1.1.20190517T102753Z.13f81bc398e9e1aa.509a1837279fd9a7593036c9f53ec2db90a2a030'){
+	constructor(text, lang = 'en-ru',  key = 'trnsl.1.1.20190517T102753Z.13f81bc398e9e1aa.509a1837279fd9a7593036c9f53ec2db90a2a030'){
 		this.text = text,
-		this.key = key
+		this.key = key,
+		this.lang = lang
 	}
 	requestYandex(url){
 		request(url,( err, res, body) => {
 			if (!err) {
 				const  item = JSON.parse(body);
-				console.log(item.text[0]);
+				console.log(item.text[0].green);
 			} else {
 				console.log(err)
 			}
@@ -27,18 +29,18 @@ class Translator {
 		params.query = {
 			key: this.key,
 			text: this.text,
-			lang: 'en-ru'
+			lang: this.lang
 		};
 		this.requestYandex(url.format(params))
 	}
 }
 
-console.log('Введите текст на английском языке и нажмите Enter.\nДля выхода наберите exit');
-rl.on('line', (answer) => {
-	if(answer === 'exit'){
+console.log('Введите текст на английском языке и нажмите Enter.\nДля выхода наберите'.yellow, 'exit'.red);
+rl.on('line', (text) => {
+	if(text === 'exit'){
 		rl.close();
 	} else {
-		const translator = new Translator(answer);
+		const translator = new Translator(text);
 		translator.urlParse()
 	}
 });

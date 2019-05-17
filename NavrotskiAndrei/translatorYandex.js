f7e262471b05f9d94e10121595508f2d02401b31
Nauronski <6602796@gmail.com>
2019-05-17 17:59:41 +0200

    lesson3Task2; JSON.parse

--- NavrotskiAndrei/translatorYandex.js
+++ NavrotskiAndrei/translatorYandex.js
@@ -1,42 +1,44 @@
-const readline = require('readline');
-const url = require('url');
 const request =  require('request');
+const url = require('url');
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
 class Translator {
-	constructor(key = 'trnsl.1.1.20190517T102753Z.13f81bc398e9e1aa.509a1837279fd9a7593036c9f53ec2db90a2a030'){
+	constructor(text, key = 'trnsl.1.1.20190517T102753Z.13f81bc398e9e1aa.509a1837279fd9a7593036c9f53ec2db90a2a030'){
+		this.text = text,
 		this.key = key
 	}
 	requestYandex(url){
 		request(url,( err, res, body) => {
 			if (!err) {
-				console.log(body);
+				const  item = JSON.parse(body);
+				console.log(item.text[0]);
 			} else {
 				console.log(err)
 			}
 		});
 	}
-	urlParse(text){
+	urlParse(){
 		const params = url.parse('https://translate.yandex.net/api/v1.5/tr.json/translate?');
 		delete params.search;
 		params.query = {
 			key: this.key,
-			text: text,
+			text: this.text,
 			lang: 'en-ru'
 		};
 		this.requestYandex(url.format(params))
 	}
 }
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-const translator = new Translator();
-console.log('Введите текст на английском языке и нажмите Enter\nДля выхода наберите exit');
+console.log('Введите текст на английском языке и нажмите Enter.\nДля выхода наберите exit');
 rl.on('line', (answer) => {
 	if(answer === 'exit'){
 		rl.close();
 	} else {
-		translator.urlParse(answer)
+		const translator = new Translator(answer);
+		translator.urlParse()
 	}
 });

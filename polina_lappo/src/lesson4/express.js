9131b9c3e15b1d8f5e93e3a1e0d499e14f048c64
Polina Lappo <polina.lappo@soft-werke.com>
2019-05-29 05:01:25 +0300

    task 1

--- polina_lappo/src/lesson4/express.js
+++ polina_lappo/src/lesson4/express.js
@@ -2,43 +2,78 @@ const path = require('path');
 
 const express = require('express');
 const consolidate = require('consolidate');
+const handlebars = require('handlebars');
 
 const app = express();
 
 const request = require('request');
+const cheerio = require('cheerio');
 
 app.engine('hbs', consolidate.handlebars);
+handlebars.registerHelper('list', function(context, options) {
+    var ret = '<div>';
+  
+    for(var i=0, j=context.length; i<j; i++) {
+      ret = ret  + options.fn(context[i]);
+    }
+  
+    return ret + '</div>';
+  });
 app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, 'view'));
 
 const nav = {
-    link1:'News',
-    link2: 'Translater'
+    link1:'News'
 };
-const api = [{
-        id: 1,
-        link: "https://lenta.ru/parts/news"
-    }]
-
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.render('home', nav);
 });
+
 app.get('*/news', (req, res) => {
     res.render('news', {});
 });
 
 
-app.post('*/read', (req, res) => {
+app.post('*/news', (req, res) => {
 
-    request('', (err, response, html) => {
-        if(!err && response.statusCode === 200) {
-
-            res.render('read', {text: html});
-        }  
-    })
+    const temp = {news: []};
+  
+    switch (req.body.source){
+        case '1':
+            request('https://lenta.ru/parts/news', (e, r, html) => {
+                if(!e && r.statusCode === 200) {
+                    const $ = cheerio.load(html);
+                    for (let i = 0; i < req.body.sourceCount; i++) {
+                        temp.news[i] = {
+                            catalog: $('#more > div.item.news > div.info.item__info.g-date > a').eq(i).text(),
+                            new: $('#more > div.item.news > div.titles > h3 > a').eq(i).text()
+                        }
+                    }
+                    res.render('read', temp);
+                }
+            });
+            break;
+        case '2':
+            request('https://www.newsru.com/allnews/', (e, r, html) => {
+                if(!e && r.statusCode === 200) {
+                    const $ = cheerio.load(html);
+                    for (let i = 0; i < req.body.sourceCount; i++) {
+                        temp.news[i] = {
+                            catalog: $('.body-page-center-column > div.index-news-item > div.index-news-content > span.index-news-date > a').eq(i).text(),
+                            new: $('.body-page-center-column > div.index-news-item > div.index-news-content > .index-news-title').eq(i).text()
+                        }
+                    }
+                    res.render('read', temp);
+                }
+            });
+            break;
+        default: 
+        console.log('error')
+    }
+    
 });
 
 app.listen(8080, () => {

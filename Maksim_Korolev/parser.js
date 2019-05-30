d5c3ba181a1ba80d1665dcdafd3dc30b1ff87dc9
KosmoPirat <kosmopirat@gmail.com>
2019-05-30 15:04:55 +0300

    done

--- Maksim_Korolev/parser.js
+++ Maksim_Korolev/parser.js
@@ -3,36 +3,41 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-export const newsData = {
-	news120: {
-		title: null,
-		content: null,
-	},
 
-	newsAnimal: {
-		title: null,
-		content: null,
-	},
-};
+function getNews120() {
+    const news = {
+        title: null,
+        content: null,
+    };
+    return new Promise((resolve, reject) => {
+        request('https://120.su/', (err, req, html) => {
+            if (!err && req.statusCode === 200) {
+                const $ = cheerio.load(html);
+                news.title = $('.entry-title');
+                news.content = $('.entry-content').find('p');
+            } else {
+                console.log('Error: ' + err);
+            }
+            resolve(news);
+        });
+    });
+}
 
-function getParsNews(newsData) {
-	request('https://120.su/', (err, req, html) => {
-		if (!err && req.statusCode === 200) {
-			const $ = cheerio.load(html);
-			newsData.news120.title = $('.entry-title');
-			newsData.news120.content = $('.entry-content').find('p');
-		} else {
-			console.log('Error: ' + err);
-		}
-	});
-
-	request('http://goodnewsanimal.ru/', (err, req, html) => {
-		if (!err && req.statusCode === 200) {
-			const $ = cheerio.load(html);
-			newsData.newsAnimal.title = $('.newsTitle').find('a');
-			newsData.newsAnimal.content = $('.newsMessage');
-		} else {
-			console.log('Error: ' + err);
-		}
-	});
+function getAnimalNews() {
+    const news = {
+        title: null,
+        content: null,
+    };
+    return new Promise((resolve, reject) => {
+        request('http://goodnewsanimal.ru/', (err, req, html) => {
+            if (!err && req.statusCode === 200) {
+                const $ = cheerio.load(html);
+                news.title = $('.newsTitle').find('a');
+                news.content = $('.newsMessage');
+            } else {
+                console.log('Error: ' + err);
+            }
+            resolve(news);
+        });
+    });
 }

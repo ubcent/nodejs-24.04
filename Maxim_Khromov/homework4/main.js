035f09e1e57811832c6b18b5815c80766fe4ad09
MaxKhromov <sloopemail@gmail.com>
2019-06-05 00:25:53 +0400

    homework 4 is ready

--- Maxim_Khromov/homework4/main.js
+++ Maxim_Khromov/homework4/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const templating = require('consolidate');
+const path = require('path')
 
 const routes = require('./routes/routes.js');
 
@@ -11,6 +12,12 @@ app.engine('hbs', templating.handlebars);
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, '/views/public')));
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
 
 routes(app);
 
--- Maxim_Khromov/homework4/routes/routes.js
+++ Maxim_Khromov/homework4/routes/routes.js
@@ -1,35 +1,52 @@
  const request = require('request');
 
-const appRouter = function (app) {
-    const newsSetting = {
-        apiKey: 'apiKey=c66ae4a42de04c80a8d61cc6040662cb',
-        language: 'country=ru',
-        newsAmount: 10,
-    }
+ const newsApiSettings = {
+     apiKey: 'apiKey=c66ae4a42de04c80a8d61cc6040662cb',
+     language: 'ru',
+     page: '1',
+ };
 
-    app.get('/', (req, res) => {
+ const languageList = [
+    'ar', 'de', 'en', 'es', 'fr', 'he', 'it', 'nl', 'no', 'pt', 'ru', 'se', 'ud', 'zh', 
+ ];
 
-        request(
-            "https://newsapi.org/v2/top-headlines?" +
-            "country=ru&" +
-            "apiKey=c66ae4a42de04c80a8d61cc6040662cb",
-            (err, response, body) => {
-                if (!err && response.statusCode === 200) {
-                    const newsApiBody = JSON.parse(body);
-                    let newsTitles = '';
-    
-                    for (let i = 0; i < (newsApiBody.totalResults - 14); i++) { // Почему-то c api приходит на 14 статей меньше, чем указано
-                        newsTitles += JSON.stringify(newsApiBody.articles[i].title) + '<br>';
-                    }
-    
-                    res.render('hello', {
-                        title: 'newsTitles'
-                    });
-                }
-            }
-        );
+ const appRouter = function (app) {
 
-    });
-}
+     app.get('/', (req, res) => {
 
-module.exports = appRouter;
+         const newsApiUrl = `https://newsapi.org/v2/top-headlines?language=${newsApiSettings.language}&${newsApiSettings.apiKey}&page=${newsApiSettings.page}`;
+
+         request(newsApiUrl, (err, response, body) => {
+             if (!err && response.statusCode === 200) {
+                 const newsApiBody = JSON.parse(body);
+
+                 (async function () {
+                     const articles = [];
+
+                     const getNewsTitles = await newsApiBody.articles.map(article => {
+                         articles.push(article);
+                     });
+                     res.render('hello', {
+                         filter: newsApiSettings,
+                         article: articles,
+                         languageList: languageList,
+                     });
+
+                 })().catch(e => {
+                     console.error(e)
+                 })
+
+             } else {
+                 return response.statusCode;
+             };
+         });
+
+     });
+
+     app.post('/', (req, res) => {
+         newsApiSettings.language = req.body.language;
+         res.redirect('/');
+     });
+ }
+
+ module.exports = appRouter;

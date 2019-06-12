b3c59de01a1346feeb489c1a1c5461706a1f57b3
hydra13 <rob.sabirov@yandex.ru>
2019-06-12 17:54:53 +0300

    added pages (login, user and registration)

--- Robert Sabirov/src/lesson6/models/user.js
+++ Robert Sabirov/src/lesson6/models/user.js
@@ -4,8 +4,15 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: { type: String },
     password: { type: String },
-    name: { type: Boolean },
-    lastVisit: { type: Date }
+    name: { type: String }
 });
 
-module.exports = mongoose.model('User', todoSchema, 'users');
+userSchema.methods.checkPassword = function (password) {
+    console.log('checkPassord');
+
+    console.log('this p: ' + this.password);
+    console.log('pass: ' + password);
+    return this.password === password;
+};
+
+module.exports = mongoose.model('User', userSchema, 'users');
--- Robert Sabirov/src/lesson6/server.js
+++ Robert Sabirov/src/lesson6/server.js
@@ -1,18 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const consolidate = require('consolidate');
+const path = require('path');
 const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 
 const app = express();
 
-app.use(cookie());
-app.use(session({ keys: ['secret'] }));
+app.engine('hbs', consolidate.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', path.resolve(__dirname, 'views'));
+
+// app.use(cookie());
+// app.use(session({ keys: ['testingServer'] }));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+
+mongoose.connect('mongodb://127.0.0.1:27017/myapp', { useNewUrlParser: true });
+
+const User = require('./models/user');
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new Strategy(async (username, password, done) => {
+    console.log(`In auth function: u - ${username}, p - ${password}`)
     const user = await User.findOne({ username });
+    console.log(`user:`);
+    console.dir(user);
     if (user && user.checkPassword(password)) {
         delete user.password;
         return done(null, user);
@@ -22,17 +40,34 @@ passport.use(new Strategy(async (username, password, done) => {
 }));
 
 passport.serializeUser((user, done) => {
+    console.log('serializeUser')
     done(null, user._id);
 });
 
 passport.deserializeUser(async (id, done) => {
+
+    console.log('deserializeUser')
     const user = await User.findById(id);
     done(null, user);
 });
 
-app.get('/auth', (req, res) => {
-    res.send('TODO: Auth form');
+app.get('/', (req, res) => {
+    res.render('loginPage');
+})
 
+app.get('/reg', (req, res) => {
+    res.render('regPage');
+})
+
+app.post('/reg', async (req, res) => {
+    let user = new User(req.body);
+    user = await user.save();
+
+    res.send(user);
+})
+
+app.get('/auth', (req, res) => {
+    res.render('loginPage');
 })
 
 const authHandler = passport.authenticate('local', {
@@ -40,7 +75,10 @@ const authHandler = passport.authenticate('local', {
     failureRedirect: '/auth'
 })
 
-app.post('/auth', authHandler);
+app.post('/auth', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth'
+}));
 
 const mustBeAuthenticated = (req, res, next) => {
     if (req.user) {
@@ -53,11 +91,13 @@ const mustBeAuthenticated = (req, res, next) => {
 app.all('/user*', mustBeAuthenticated);
 
 app.get('/user', (req, res) => {
-    res.send('TODO: User profile');
+    res.render('userPage', {
+        user: req.user.name
+    })
 })
 
 app.get('/user/settings', (req, res) => {
-    res.send('TODO: User settings');
+    res.send('TODO: User settings for ' + req.user.name);
 })
 
 app.get('/logout', (req, res) => {
@@ -65,6 +105,6 @@ app.get('/logout', (req, res) => {
     res.redirect('/auth');
 })
 
-app.listen(4444, () => {
-    console.log('Server has been started (port 4444)');
+app.listen(13013, () => {
+    console.log('Server has been started (port 13013)');
 })

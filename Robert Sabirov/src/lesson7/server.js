e7fb6f76a73ab84b02e14c9ef209584401d18625
hydra13 <rob.sabirov@yandex.ru>
2019-06-19 19:39:13 +0300

    completed (without testing)

--- Robert Sabirov/src/lesson7/server.js
+++ Robert Sabirov/src/lesson7/server.js
@@ -2,10 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const mongoose = require('mongoose');
-const consolidate = require('consolidate');
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const session = require('cookie-session');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
@@ -14,74 +11,21 @@ const config = require('./config');
 
 const app = express();
 
-app.engine('hbs', consolidate.handlebars);
-app.set('view engine', 'hbs');
-app.set('views', path.resolve(__dirname, 'views'));
-
-app.use(session({ keys: ['testingServer'] }));
-
 app.use(express.json());
 app.use(cors());
-
-app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
 mongoose.connect(`mongodb://${config.hostDb}:${config.portDb}/${config.dbName}`, { useNewUrlParser: true });
 
 const User = require('./models/user');
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(async (username, password, done) => {
-    const user = await User.findOne({ username });
-    if (user && user.checkPassword(password)) {
-        delete user.password;
-        return done(null, user);
-    } else {
-        return done(null, false);
-    }
-}));
-
-passport.serializeUser((user, done) => {
-    done(null, user._id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    const user = await User.findById(id);
-    done(null, user);
-});
-
-app.get('/', (req, res) => {
-    res.render('loginPage');
-})
-
-// app.get('/reg', (req, res) => {
-//     res.render('regPage');
-// })
-
-// app.post('/reg', async (req, res) => {
-//     let user = new User(req.body);
-//     user = await user.save();
-
-//     res.send(user);
-// })
-
-app.get('/auth', (req, res) => {
-    res.render('loginPage');
-})
-
-const redirestions = {
-    successRedirect: '/user',
-    failureRedirect: '/auth'
-};
-
-// app.post('/auth', passport.authenticate('local', redirestions));
-
-app.post('/auth', (req, res) => {
+app.post('/auth', async (req, res) => {
     const { username, password } = req.body;
 
-    if (username === 'admin' && password === 'admin') {
-        const token = jwt.sign({ name: 'Administrator', surname: '' }, 'secretString');
+    const user = await User.findOne({ username });
+
+    if (user && user.checkPassword(password)) {
+        delete user.password;
+        const token = jwt.sign({ name: user.name, username: user.username }, 'secretString');
         res.json({ token });
     } else {
         res.status(401).json({ message: 'Wrong credentials' });
@@ -95,36 +39,46 @@ const verifyToken = (req, res, next) => {
             if (err) {
                 res.status(401).json({ message: 'Wrong token' });
             }
+            req.user = decoded;
+            next();
         });
     } else {
         res.status(401).json({ message: 'No token present' });
     }
 }
 
-const mustBeAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect('/auth');
-    }
-}
+app.all('/users*', verifyToken);
 
-app.all('/user*', mustBeAuthenticated);
+app.get('/users', async (req, res) => {
+    const users = await User.find();
 
-app.get('/user', (req, res) => {
-    res.render('userPage', {
-        user: req.user.name
-    })
-})
+    res.json(users);
+});
 
-app.get('/user/settings', (req, res) => {
-    res.send('TODO: User settings for ' + req.user.name);
-})
+app.post('/users', async (req, res) => {
+    let user = new User(req.body);
+    user = await user.save();
 
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/auth');
-})
+    res.send(user);
+});
+
+app.put('/users/:id', async (req, res) => {
+    const user = await User.findOneAndUpdate(req.params.id, req.body);
+
+    res.json(user);
+});
+
+app.patch('/users/:id', async (req, res) => {
+    const user = await User.findOneAndUpdate(req.params.id, { $set: req.body });
+
+    res.json(user);
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id, req.body);
+
+    res.send(user);
+});
 
 app.listen(config.port, () => {
     console.log(`Server has been started (port ${config.port})`);
--- Robert Sabirov/src/lesson7/testing.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i < 999999999999999999999999; i += 10000) {
-    if (i % 5000000000 === 0)
-        console.log(i);
-}
-console.log('job is done!');

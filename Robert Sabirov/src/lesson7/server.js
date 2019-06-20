4c38fdb19fe50195ff498cca68fd35746685a9fe
hydra13 <rob.sabirov@yandex.ru>
2019-06-20 08:43:10 +0300

    added refresh-tokens

--- Robert Sabirov/src/lesson7/server.js
+++ Robert Sabirov/src/lesson7/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const session = require('cookie-session');
 const jwt = require('jsonwebtoken');
+const randtoken = require('rand-token');
 const cors = require('cors');
 
 const config = require('./config');
@@ -15,8 +16,31 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(`mongodb://${config.hostDb}:${config.portDb}/${config.dbName}`, { useNewUrlParser: true });
-
 const User = require('./models/user');
+
+const refreshTokens = {};
+
+app.post('/auth/token/reject', function (req, res, next) {
+    const { refresh_token } = req.body;
+    if (refresh_token in refreshTokens) {
+        delete refreshTokens[refresh_token];
+        res.sendStatus(204);
+    } else {
+        res.send(401).json({ message: 'Wrong refreshTokens' });
+    }
+})
+
+app.post('/auth/token', async (req, res) => {
+    const { username, refresh_token } = req.body;
+    if ((refresh_token in refreshTokens) && (refreshTokens[refresh_token] == username)) {
+        const user = await User.findOne({ username: username });
+        const token = jwt.sign({ name: user.name, username: user.username }, 'secretString');//, { expiresIn: 300 })
+        res.json({ access_token: token });
+    }
+    else {
+        res.send(401).json({ message: 'Wrong refreshTokens' });
+    }
+})
 
 app.post('/auth', async (req, res) => {
     const { username, password } = req.body;
@@ -25,8 +49,10 @@ app.post('/auth', async (req, res) => {
 
     if (user && user.checkPassword(password)) {
         delete user.password;
-        const token = jwt.sign({ name: user.name, username: user.username }, 'secretString');
-        res.json({ token });
+        const token = jwt.sign({ name: user.name, username: user.username }, 'secretString')//; { expiresIn: 30 });
+        const refresh_token = randtoken.uid(256);
+        refreshTokens[refresh_token] = user.username;
+        res.json({ access_token: token, refresh_token: refresh_token });
     } else {
         res.status(401).json({ message: 'Wrong credentials' });
     }
